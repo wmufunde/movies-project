07c42e4de9043b3e7f7f17d9dc0a70d65197e932
wmufunde <wmmufunde@yahoo.com>
2017-09-21 16:26:27 +0100

    initial-commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/* THIS IS THE JS FILE FOR THE www.themoviedb.org WEBSITE API */
+
+// MY GLOBAL VARIABLES
+var title
+var genre
+var summary
+var actors 
+var languages // What languges is the movie in?
+var status // Is this movie releases or still in production?
+
+
+//HERE ARE MY EVEN LISTENERS
+
+var getDetails = document.getElementById()
+
+
+
+/* ----------------------------------------------------------------------------
+YOUTUBE API for trailer
